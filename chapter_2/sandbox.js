2722218538799fb77281dafda3586d15e116435d
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-07-03 15:14:13 +0800

    Done Chapter 3

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,215 +1,172 @@
-// // strings
-// console.log('hello, world')
-
-// let email = 'crisaianvergara@gmail.com'
-// console.log(email);
-
-// // string concatenation
-// let firstName = 'Brandon';
-// let lastName = 'Brad';
-
-// let fullName = firstName + ' ' + lastName
-// console.log(fullName);
-
-// // getting characters
-// console.log(fullName[2]);
-
-// // string length
-// console.log(fullName.length);
-
-// // string methods
-// console.log(fullName.toUpperCase());
-// let result = fullName.toLocaleLowerCase();
-// console.log(result, fullName);
-
-// let index = email.indexOf("@");
-// console.log(index);
-
-// common string methods
-// let email = "crisaianvergara@gmail.com";
-// // let result = email.lastIndexOf('n');
-
-// // let result = email.slice(0,10);
-
-// // let result = email.substring(4, 10);
-
-// // let result = email.replace('m', 'w');
-
-// let result = email.replace('n', 'w');
-
-// console.log(result);
-
-// let radius = 10;
-// const pi = 3.14;
-
-// console.log(radius, pi);
-
-// math operators +, -, *, /, **, %
-
-// console.log(10 / 2)
-// let result = radius % 3;
-
-// let result = pi * radius**2;
-
-// order of operation = B I D M A S
-
-// let result = 5 * (10-3)**2
-
-// console.log(result)
-
-// let likes = 10;
-
-// likes = likes + 1;
-// likes++;
-// likes--;
-// likes += 10;
-// likes -= 5;
-// likes *= 2;
-// likes /= 2;
-
-// console.log(likes)
-
-// NaN - not a number
-
-// console.log(5 / 'hello');
-// console.log(5 * 'hello');
-
-// let result = 'the blog has ' + likes + ' likes';
-
-// console.log(result);
-
-// template strings
-// const title = 'Best reads of 2019';
-// const author = 'Mario'
-// const likes = 30;
-
-// // concatenation way
-// // let result = 'The blog called ' + title + ' by ' + author + 'has ' + likes + ' likes';
-
-// // template string way
-// let result = `The blog called ${title} by ${author} has ${likes} likes.`;
-
-// console.log(result);
-
-// // creating html template
-// let html = `
-//     <h2>${title}</h2>
-//     <p>${author}</p>
-//     <span>This blog has ${likes} likes</span>
-// `;
-
-// console.log(html);
-
-// let ninjas = ['shawn', 'ryu', 'chun-li'];
-
-// ninjas[1] = 'ken';
-// console.log(ninjas[1]);
-
-// let ages = [20, 25, 30, 35];
-// console.log(ages[2]);
-
-// let random = ['shawn', 'krystal', 30, 20];
-// console.log(random);
-
-// console.log(ninjas.length);
-
-// array methods
-// let result = ninjas.join('-');
-// let result = ninjas.indexOf('chun-li');
-// let result = ninjas.concat(['ken', 'krystal']);
-// like append in python
-// let result = ninjas.push('ken');
-// // remove the last item of an array
-// result = ninjas.pop();
-// console.log(ninjas);
-
-// NULL and UNDEFINED
-// let age = null;
-// console.log(age, age + 3, `the age is ${age}`)
-
-// Booleans and comparisons
-// console.log(true, false, "true", "false")
-
-// let email = 'luigi@gmail.com';
-// let names = ['luigi', 'mario', 'toad'];
-
-// look the value in a variable
-// let result = email.includes('@');
-// let result = names.includes('luigi');
-// console.log(result);
-
-// comparison operators
-// let age = 25;
-
-// console.log(age == 25);
-// console.log(age == 30);
-// console.log(age != 30);
-// console.log(age != 25);
-// console.log(age > 20);
-// console.log(age < 20);
-// console.log(age >= 25);
-// console.log(age <= 25);
-
-// let name1 = 'shaun';
-
-// console.log(name1 == 'shaun');
-// console.log(name1 == 'Shaun');
-// console.log(name1 > 'crystal');
-// console.log(name1 > 'Shaun');
-// console.log(name1 > 'Crystal');
-
-// loose comparison (different types can still be equal)
-// console.log(age == 25);
-// console.log(age == '25');
-// console.log(age != 25);
-// console.log(age != '25');
-
-// strict comparison (different types can still be equal)
-// console.log(age === 25);
-// console.log(age === '25');
-// console.log(age !== 25);
-// console.log(age !== '25');
-
-// type conversion
-// let score = '100';
-
-// concat
-// console.log(score + 1);
-
-// change the type from string to integer
-// score = Number(score);
-// console.log(score + 1);
-
-// show variable type
-// console.log(typeof score);
-
-// NaN
-// let result = Number('hello')
-// console.log(result);
-
-// change the type from number to string
-// let result = String(50);
-// console.log(result, typeof result);
-
-// let result = Boolean(50);
-// let result = Boolean(0);
-let result = Boolean('');
-
-console.log(result, typeof result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// for loops
+// for(let i = 0; i < 5; i++) {
+//     console.log('in loop: ', i);
+// }
+// console.log('loop finished');
+
+
+// const names = ['shaun', 'mario', 'luigi'];
+
+// for(let i = 0; i < names.length; i++) {
+//     // console.log('name: ', names[i]);
+//     let html = `<div>${names[i]}</div>`
+//     console.log(html);
+// }
+// console.log('loop finished');
+
+// while loops
+// const names = ['shaun', 'mario', 'luigi'];
+
+// let i = 0;
+
+// while (i < 5) {
+//     console.log('in loop', i);
+//     i++;
+// }
+
+// while(i < names.length) {
+//     console.log(names[i]);
+//     i++;
+// }
+
+// do while loops
+// let i = 0;
+// do {
+//     console.log('val of i is: ', i);
+//     i++;
+// } while (i < 5);
+
+// if statements
+// const age = 21;
+// if(age > 20) {
+//     console.log('You are over 20 years old');
+// };
+
+// const names = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+// if (names.length > 3) {
+//     console.log("That's a lot of ninjas");
+// }
+
+// const password = 'password';
+
+// if (password.length >= 8) {
+//     console.log("That password is long enough");
+// };
+
+// else if statements
+// const password = 'passworasfsafgfafasfasf';
+
+// if (password.length >= 12) {
+//     console.log("That password is mighty strong");
+// } else if (password.length >= 8) {
+//     console.log("That password is long enough");
+// } else {
+//     console.log("That password is not long enough");
+// }
+
+// logical operators - OR || and AND &&
+// const password = 'password';
+
+// if (password.length >= 12 && password.includes("@")) {
+//     console.log("That password is mighty strong");
+// } else if (password.length >= 8 || password.includes("@") && password.length > 5) {
+//     console.log("That password is strong enough");
+// } else {
+//     console.log("That password is not long enough");
+// }
+
+// logical NOT (!)
+// let user = false;
+
+// if(!user) {
+//     console.log("Hello");
+// }
+// console.log(!true);
+// console.log(!false);
+
+// break and continue
+// const scores = [50, 25, 0, 30, 100, 20, 10];
+
+// for (let i = 0; i < scores.length; i++) {
+
+//     if(scores[i] === 0) {
+//         continue;
+//     }
+
+//     console.log('your score: ' + scores[i]);
+
+//     if(scores[i] === 100) {
+//         console.log("Congrats, you got the top score!")
+//         break;
+//     }
+// }
+
+// switch statements
+// const grade = 'D';
+
+// switch (grade) {
+//     case 'A':
+//         console.log('You got an A!');
+//         break;
+//     case 'B':
+//         console.log('You got B!');
+//         break;
+//     case 'C':
+//         console.log('You got C!');
+//         break;
+//     case 'D':
+//         console.log('You got D!');
+//         break;
+//     case 'E':
+//         console.log('You got an E!');
+//         break;
+//     default:
+//         console.log('Not a valid grade!');
+// }
+
+// using if statements
+// if(grade === 'A') {
+
+// } else if(grade === 'B') {
+
+// } else if(grade === 'C') {
+    
+// } else if(grade === 'D') {
+    
+// } else if(grade === 'E') {
+    
+// }
+
+
+// variables & block scope
+// let age = 30;
+
+// if(true) {
+//     let age = 40;
+//     let name1 = 'shaun';
+//     console.log('inside 1st code block', age, name1);
+
+//     if(true) {
+//         let age = 50;
+//         console.log('inside 2nd code block', age);
+//     }
+// }
+
+// console.log('outside code block', age);
+
+const age = 30;
+
+if(true) {
+    const age = 40;
+    const name1 = 'shaun';
+    console.log('inside 1st code block', age, name1);
+
+    if(true) {
+        const age = 50;
+        console.log('inside 2nd code block', age);
+        var test = 'hello world';
+    }
+}
+
+console.log('outside code block', age, test);
